ee052e8370195de3d831c617f8d197ce6fbbe063
yasminemanuely14 <yasmin.lima.santos14@escola.pr.gov.br>
2026-08-14 12:30:46 -0300

    index

---
+++ script.js
@@ -0,0 +1,14 @@
+const botao = document.querySelector("button");
+botao.addEventListener("click", botaoClicado);
+function botaoClicado() {
+console.log("fui clicado");
+}
+function botaoClicado() {
+    console.log("fui clicado");
+    let texto = botao.querySelector("span");
+}
+function botaoClicado() {
+    console.log("fui clicado");
+    let texto = botao.querySelector("span");
+    texto.textContent++;
+}
